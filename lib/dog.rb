c4a1696346cdbcafb6c100429b3ba161621424df
Andres Carvajal <jc1993box@hotmail.com>
2020-02-10 03:01:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,16 @@
-class Dog 
- fido = Dog.new
-  def fido.name = "fido"
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
-  
-  def .name=(dog_name)
-  end
-  
-  .name
-  
-  def .bark
-    puts "woof!"
+ 
+  def name
+    @this_dogs_name
   end
 end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
+ 
